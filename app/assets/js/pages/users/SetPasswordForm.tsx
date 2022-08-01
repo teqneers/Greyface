029cea7fbf9568273f2fdbb220f0fe190e8ea56c
Simranjit Kaur <simranjit@teqneers.de>
2022-08-01 09:25:18 +0200

    Improve change password by adding password retype field and min char length

--- app/assets/js/pages/users/SetPasswordForm.tsx
+++ app/assets/js/pages/users/SetPasswordForm.tsx
@@ -1,3 +1,4 @@
+import {TFunction} from 'i18next';
 import React from 'react';
 import {UseMutationResult} from 'react-query';
 import {useTranslation} from 'react-i18next';
@@ -9,21 +10,31 @@ import CancelButton from '../../controllers/Buttons/CancelButton';
 
 export interface SetPasswordValues {
     password: string,
+    passwordConfirmation: string
 }
 
 export interface SetPasswordRequest {
     password: string
 }
 
-const Schema: yup.SchemaOf<SetPasswordValues> = yup.object()
-    .noUnknown()
-    .shape(
-        {
-            password: yup.string()
-                .required()
-                .max(128)
-        }
-    );
+function Schema(t: TFunction): yup.AnySchema {
+    return yup.object()
+        .noUnknown()
+        .shape(
+            {
+                password: yup.string()
+                    .required()
+                    .min(5, t('errors.min', {min: 5}))
+                    .max(128),
+                passwordConfirmation: yup.string()
+                    .required()
+                    .when("password", {
+                        is: password => (password && password.length > 0),
+                        then: yup.string().oneOf([yup.ref("password")], "Password doesn't match")
+                    })
+            }
+        );
+}
 
 
 interface UserFromProps<TValues extends object, TData, TRes, TError> {
@@ -45,7 +56,7 @@ function SetPasswordForm<TValues extends SetPasswordValues, TData extends SetPas
     const {t} = useTranslation();
     return (
         <Formik
-            validationSchema={Schema}
+            validationSchema={Schema(t)}
             onSubmit={((values, {setFieldError}) => {
                 const submitData = {password: values.password.trim()};
                 if (submitData.password.length > 0) {
@@ -78,6 +89,21 @@ function SetPasswordForm<TValues extends SetPasswordValues, TData extends SetPas
 
                                 <Form.Control.Feedback type="invalid">
                                     {errors.password}
+                                </Form.Control.Feedback>
+                            </Form.Group>
+
+                            <Form.Group as={Col} md="12" controlId="validationFormik04">
+                                <Form.Label>{t('user.passwordRetype')}</Form.Label>
+
+                                <Form.Control
+                                    type="text"
+                                    name="passwordConfirmation"
+                                    value={values.passwordConfirmation}
+                                    onChange={handleChange}
+                                    isInvalid={!!errors.passwordConfirmation}/>
+
+                                <Form.Control.Feedback type="invalid">
+                                    {errors.passwordConfirmation}
                                 </Form.Control.Feedback>
                             </Form.Group>
 
