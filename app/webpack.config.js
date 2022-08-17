30fecd1849aac4402753ce66bf5c11cca2651383
Simranjit Kaur <simranjit@teqneers.de>
2022-08-17 15:41:15 +0200

    Webpack menifest prefix

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -14,7 +14,7 @@ Encore
     // public path used by the web server to access the output path
     .setPublicPath(publicPath)
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    .setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
