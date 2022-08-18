beabfc7c29c4483607db796a378675f72b60bbb8
Simranjit Kaur <simranjit@teqneers.de>
2022-08-18 09:35:13 +0200

    Removed set default timezone

--- app/public/index.php
+++ app/public/index.php
@@ -3,7 +3,7 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-date_default_timezone_set("Europe/Berlin");
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
