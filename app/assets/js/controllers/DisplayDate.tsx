23f9a8cad533dc318dbf51f2a0e4ec3eba3e19fe
Simranjit Kaur <simranjit@teqneers.de>
2022-08-18 10:23:04 +0200

    Tried to fix date issue

--- app/assets/js/controllers/DisplayDate.tsx
+++ app/assets/js/controllers/DisplayDate.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import {DATE_TIME_FORMAT, DateObject} from '../types/common';
-import {format as dateFormat} from 'date-fns';
+
+import {format as dateFormat, isValid} from 'date-fns';
 
 interface DisplayDateProps {
     date: DateObject,
@@ -10,8 +11,14 @@ interface DisplayDateProps {
 
 const DisplayDate: React.VFC<DisplayDateProps> = ({date, format = DATE_TIME_FORMAT}) => {
 
+    const d = new Date(date.date);
+
+   if (!isValid(d)) {
+       console.log(date, d);
+   }
+
     return (
-        <span>{dateFormat(new Date(date.date), format)}</span>
+        <span>{isValid(d) ? dateFormat(d, format) : '-' }</span>
     );
 };
 
