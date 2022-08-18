bf5e70ecc6e1ce0abcb4259a4ba581b8de1ac05f
Simranjit Kaur <simranjit@teqneers.de>
2022-08-18 13:03:54 +0200

    Fixed header items align

--- app/assets/js/application/ApplicationContainer.tsx
+++ app/assets/js/application/ApplicationContainer.tsx
@@ -39,7 +39,7 @@ const ApplicationContainer: React.FC = ({children}) => {
                         <NavDropdown
                             title={<><img
                                 src={require(`../../images/language/${flag}.png`)}
-                                className="d-inline-block m-1" alt={flag}/>
+                                className="d-inline-block" alt={flag}/>
                                 {t(`locale.${locale.replace('-', '_')}`)}</>}>
 
                             {Object.entries(SettingsLocale).map(([key, value]) => {
