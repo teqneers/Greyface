6f167f0943340d1b0d5b18e75bf6bd78fc8a56be
Simranjit Kaur <simranjit@teqneers.de>
2024-07-04 14:12:30 +0200

    Added build with tag command

---
+++ app/files/deploy/make.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# ERROR HANDLING
+#set -o pipefail  # trace ERR through pipes
+set -o errexit  # set -e : exit the script if any statement returns a non-true return value
+set -o errtrace # trace ERR through 'time command' and other functions
+set -o nounset  ## set -u : exit the script if you try to use an uninitialised variable
+
+# trap and and call function (e.g. cleanup or something like that)
+trap "handleError" SIGHUP SIGINT SIGQUIT SIGABRT SIGTERM
+
+#
+# FUNCTIONS
+#
+handleError() {
+    echo >&2 -e "\n\nAn error occured. Build was interupted"
+    exit 99
+}
+
+scriptPath="$(cd "$(dirname "$0")" && pwd -P)"
+
+#
+# Greyface
+#
+cd "${scriptPath}/../../"
+if [ -d public/build ]; then
+    rm -rf public/build/*
+fi
+if [ -d node_modules ]; then
+    rm -rf node_modules/*
+fi
+npm install --verbose && npm run build
+
+# vim: syntax=sh ts=4 sw=4 sts=4 sr noet
