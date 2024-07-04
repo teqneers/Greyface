8c7eb4aca46242e1b37dcdeb502841b2768b97db
Simranjit Kaur <simranjit@teqneers.de>
2024-07-04 11:23:47 +0200

    Build.sh changes revert

--- build.sh
+++ build.sh
@@ -21,10 +21,6 @@ popd >/dev/null
 dryRun=false
 local=0
 
-
-source "${SCRIPT_PATH}/docker/common/_env_loader.sh"
-loadEnv
-
 # define color variable to be used in echo, cat, ...
 if [[ -n "${TERM:-}" && ${TERM} != "dumb" ]]; then
     readonly C_BLACK="$(tput setaf 0)"          # Black
