07651094b2857687b7fafbfd3319edfdc2d465ba
Simranjit Kaur <simranjit@teqneers.de>
2024-08-12 15:24:10 +0200

    adds app version file

---
+++ app/version.php
@@ -0,0 +1,9 @@
+<?php
+
+#############################################################################
+###     APPLICATION SPECIFIC CONFIG
+#############################################################################
+
+// MUST BE CHANGED WITH ANY DEPLOYMENT
+define('PRODUCT_VERSION', '2.0.0');
+define('PRODUCT_BUILD', '0');    // DO NOT CHANGE MANUALLY. NEEDED BY PHING
