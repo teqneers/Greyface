43a44707d21daa6a1c4244db2fe02d5481b911dc
Simranjit Kaur <simranjit@teqneers.de>
2024-08-13 09:19:50 +0200

    updates build file and gitignore

--- app/files/build_tag.sh
+++ app/files/build_tag.sh
@@ -187,17 +187,6 @@ if [[ ! -e "${clone}/app/vendor" ]]; then
     composer install -d "${clone}/app"
 fi
 
-
-if [[ ! -e "${clone}/var" ]]; then
-    echo -e "\n-- create var folder"
-    varDir="${clone}/var"
-    cacheDir="${varDir}/cache"
-    logDir="${varDir}/log"
-    mkdir -p "${varDir}"
-    mkdir -p "${cacheDir}"
-    mkdir -p "${logDir}"
-fi
-
 echo -e "\n- generate new build artifacts"
 "${clone}"/app/files/deploy/make.sh
 
@@ -209,10 +198,6 @@ echo -e "\n- commiting and pushing artifacts"
 git add -v -f "${clone}"/app/public/build
 
 git commit -a -m "adds built javascript application for version ${tag} (${NOW})"
-
-echo -e "\n- commiting and pushing var folder"
-git add -v -f "${clone}"/var
-git commit -a -m "adds var folder for version ${tag} (${NOW})"
 
 echo -e "\n- remove all files not needed for production"
 rm -rf "${clone}/app/tests/"
